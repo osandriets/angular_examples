26fead98472073b4620057400206eaabb88a01ff
Olha Andriiets <osandriets@gmail.com>
2024-11-30 19:59:05 +0100

    init material theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatSlideToggleModule } from "@angular/material/slide-toggle";
+import { MatButtonModule } from "@angular/material/button";
+import { MatDividerModule } from "@angular/material/divider";
+import { MatIconModule } from "@angular/material/icon";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatSlideToggleModule,
+
+    MatButtonModule, MatDividerModule, MatIconModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
